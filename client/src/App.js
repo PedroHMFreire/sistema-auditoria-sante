d1069a4ff1f642a482a9514e323adcfb348692e1
PedroHMFreire <pedrohfreire@gmail.com>
2025-03-20 14:06:48 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PastCounts from './PastCounts';
-import ActiveCounts from './ActiveCounts'; // Verifique se o nome está correto
+import ActiveCounts from './ActiveCounts';
 import './App.css';
 
 function App() {
