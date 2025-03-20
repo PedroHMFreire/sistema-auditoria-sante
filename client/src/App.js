a967b5512ec19f7dfbec7702533e44d3fe4511a2
PedroHMFreire <pedrohfreire@gmail.com>
2025-03-20 19:25:00 -0300

    Atualizar o App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ActiveCount from './ActiveCount'; // Corrigido: ActiveCounts -> ActiveCount
+import ActiveCounts from './ActiveCounts';
 import PastCounts from './PastCounts';
 import './App.css';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ActiveCount />} /> {/* Corrigido: ActiveCounts -> ActiveCount */}
+        <Route path="/" element={<ActiveCounts />} />
         <Route path="/past-counts" element={<PastCounts />} />
       </Routes>
     </Router>
