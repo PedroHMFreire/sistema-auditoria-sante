9f4c4a9098fddac32c6f2f1e4b11cba4c1e1a93f
PedroHMFreire <pedrohfreire@gmail.com>
2025-03-20 21:30:04 -0300

    Atualizar o App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ActiveCount from './ActiveCount'; // Correto: usando ActiveCount (sem "S")
+import ActiveCount from './ActiveCount';
+import CreatedCounts from './CreatedCounts';
+import CountDetail from './CountDetail';
 import PastCounts from './PastCounts';
 import './App.css';
 
@@ -10,13 +12,17 @@ function App() {
       <header className="App-header">
         <h1 className="app-title">AUDITÊ</h1>
         <nav>
-          <a href="/past-counts" className="nav-link">Ver Contagens Salvas</a>
+          <a href="/" className="nav-link">Criar Contagem</a>
+          <a href="/created-counts" className="nav-link">Contagens Criadas</a>
+          <a href="/past-counts" className="nav-link">Contagens Finalizadas</a>
         </nav>
       </header>
       <main className="App-main">
         <Router>
           <Routes>
-            <Route path="/" element={<ActiveCount />} /> {/* Correto: usando ActiveCount */}
+            <Route path="/" element={<ActiveCount />} />
+            <Route path="/created-counts" element={<CreatedCounts />} />
+            <Route path="/count/:id" element={<CountDetail />} />
             <Route path="/past-counts" element={<PastCounts />} />
           </Routes>
         </Router>
